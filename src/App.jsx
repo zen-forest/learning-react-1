a29bad3b85b14db12beb1e72476c58ee651142eb
Tim Collins <38574258+zen-forest@users.noreply.github.com>
2024-04-24 15:56:21 -0400

    completed the first part of the tutorial series

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import Food from './Food.jsx'
 
 function App() {
   
   return(
     <>
     <Header/>
+    <Food/>
     <Footer/>
     </>
   );
